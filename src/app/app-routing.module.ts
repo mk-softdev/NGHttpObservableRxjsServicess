fbe7a8ca8f8a9375a246c04c9f625412ddef4ca2
‘Manas <manasp.softdev@gmail.com>
2021-09-07 17:30:26 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path:'employee-list',component:EmployeeListComponent},
+	{path:'employee-detail',component:EmployeeDetailComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/employee.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { iEmployee } from 'src/assets/data/iEmployee';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  _url: string = "/assets/data/employee.json";
+
+
+  constructor(private _http:HttpClient) { }
+  getEmployee():Observable<iEmployee[]>{
+    return this._http.get<iEmployee[]>(this._url);
+  }
+}
